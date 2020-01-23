e72966e84c4e153d5e858bbdfc6952d20e3220f7
Steven Rodriguez <srodr069@ucr.edu>
2020-01-23 11:36:04 -0800

    initializing repositor

---
+++ source/main.c
@@ -0,0 +1,37 @@
+/*	Author: stevenrodriguez
+ *  Partner(s) Name: Anthony Pham
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF ; PORTB = 0x00;
+    unsigned char led = 0x00;
+    unsigned char button = 0x00;
+
+    /* Insert your solution below */
+    while (1) {
+    	//PORTB = 0x0F;
+    	if(button){
+    		led = (led & 0xFC) | 0x01;
+    	}
+
+    	else {
+    		led = (led & 0xFC) | 0x02;
+    	}
+
+    	PORTB = led;
+
+    }
+    return 1;
+}
